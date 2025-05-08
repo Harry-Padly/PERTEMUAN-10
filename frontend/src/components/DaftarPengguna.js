33fb22a5761ae311987c82720a8a0571093a4bfc
harrypadly <h644958@gmail.com>
2025-05-08 20:54:23 +0700

    Initial commit – upload project frontend dan backend

---
+++ frontend/src/components/DaftarPengguna.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DaftarPengguna = ({ pengguna }) => {
+  return (
+    <ul>
+      {pengguna.map((p) => (
+        <li key={p.id}>{p.nama} - {p.umur} tahun</li>
+      ))}
+    </ul>
+  );
+};
+
+export default DaftarPengguna;
